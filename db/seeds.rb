636f5f96c80684addac35e819d732886969e5825
zora dong <zoradongccc@gmail.com>
2022-08-04 11:25:17 +0800

    add seeds to two models

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,31 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+stories = [
+  {author: "zora", title: "Why won't my title center?"},
+  {author:"zora", title: "css override is driving me crazy"},
+  {author:"Aggy", title: "Wechat devtool is not behaving."},
+  {author:"Lilian", title: "!%s^&W2i...."}
+]
+stories.each do |story|
+  sto = Story.new(story)
+  sto.content = Faker::Lorem.paragraph
+  if sto.save
+    p "create new seed for #{sto.title}"
+  else
+    p "create new seed fails for #{sto.title}!!!"
+  end
+end
+
+
+Story.all.each do |story|
+  (1..4).to_a.sample.times do
+    co = Comment.new(name: Faker::TvShows::GameOfThrones.character, content: Faker::TvShows::GameOfThrones.quote, story: story)
+    # story.comments.push
+    if co.save
+      p "new comment for #{story.title}: #{co.content}"
+    else
+      p "fails"
+    end
+  end
+end
